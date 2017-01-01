f0b130857406ab2c10522257aede1642138c05e9
Jasmine2 <youjingqiang@vip.qq.com>
2017-01-01 23:01:30 +0800

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>@yield('title', 'Laravel') - by Jasmine2</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
